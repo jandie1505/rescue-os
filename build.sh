df4cdcd5735a73c2f9839536003da28614310b9f
jandie1505 <34031551+jandie1505@users.noreply.github.com>
2025-09-05 09:10:16 +0200

    added auto/config file instead of the long config command in the build.sh, and added cd to live-build directory from the script location and not from the current workdir of the user

--- build.sh
+++ build.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-cd live-build
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "$SCRIPT_DIR/test" || exit 1
 
 sudo lb clean
-lb config \
-  --mode debian \
-  --distribution trixie \
-  --debian-installer none \
-  --binary-images iso-hybrid \
-  --bootappend-live "boot=live components quiet splash toram" \
-  --linux-flavours amd64
+lb config
 sudo lb build
 
